01d0674d8868ff4f92ee0eee1fd51457294dadb2
Fred Besteman <fred@besteman.net>
2020-04-05 21:24:15 -0400

    duration work

--- src/commands/gen.ts
+++ src/commands/gen.ts
@@ -1,6 +1,5 @@
-import {Command, flags} from '@oclif/command'
-import {createFile} from '../gen'
-import cli from 'cli-ux'
+import { Command, flags } from '@oclif/command'
+import { createFile } from '../gen'
 import * as inquirer from 'inquirer'
 
 // TODO: make all options available to set as cli args
@@ -10,41 +9,47 @@ export default class FileGen extends Command {
   static description = 'generate'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    configure: flags.string({char: 'c', description: 'configure'}),
+    help: flags.help({ char: 'h' }),
+    configure: flags.string({ char: 'c', description: 'configure' }),
   }
 
   async run() {
     const responses: any = await inquirer.prompt([
       {
-        name: 'projectName',
+        name: 'prjctNme',
         message: 'project name:',
         type: 'input',
       },
-      // {
-      //   name: 'startDt',
-      //   message: 'What date should the files start at?',
-      //   type: 'input',
-      // },
       {
-        name: 'fileFormat',
-        message: 'choose file format:',
+        name: 'fileNameFrmt',
+        message: 'choose file naming format:',
         type: 'list',
         choices: [
-          {name: 'wk_yyyy', value: 'wk_yyyy'},
-          {name: 'mm_dd_yyyy', value: 'mm_dd_yyyy'},
-          {name: 'yyyy_mm_dd', value: 'yyyy_mm_dd'},
+          { name: 'wk_yyyy', value: 'ww_yyyy' },
+          { name: 'MM_dd_yyyy', value: 'MM_dd_yyyy' },
+          { name: 'yyyy_MM_dd', value: 'yyyy_MM_dd' },
         ],
       },
       {
-        name: 'headingFormat',
+        name: 'duration',
+        message: 'duration (how many files?):',
+        type: 'list',
+        choices: [
+          { name: '100 days', value: '100 days' },
+          { name: 'year', value: 'year' },
+          { name: 'month', value: 'month' },
+          { name: 'week', value: 'week' },
+        ],
+      },
+      {
+        name: 'headingFrmt',
         message: 'choose heading format:',
         type: 'list',
         choices: [
-          {name: 'day', value: 'day'},
-          {name: 'date', value: 'date'},
-          {name: 'day number', value: 'day number'},
-          {name: 'star date', value: 'star date'},
+          { name: 'day', value: 'day' },
+          { name: 'date', value: 'date' },
+          { name: 'day number', value: 'day number' },
+          { name: 'star date', value: 'star date' },
         ],
       },
       {
@@ -54,10 +59,10 @@ export default class FileGen extends Command {
       },
     ])
 
-    const {confirmGen, fileFormat, headingFormat, projectName} = responses
+    const { confirmGen, duration, fileNameFrmt, headingFrmt, prjctNm } = responses
 
     if (confirmGen) {
-      createFile({prjctNm: projectName, fileFrmt: fileFormat, headingFrmt: headingFormat})
+      createFile({ prjctNm, fileNameFrmt, headingFrmt })
       this.log(responses)
     } else {
       this.log('cancelled')
--- src/gen/index.ts
+++ src/gen/index.ts
@@ -1,24 +1,44 @@
 import * as fs from 'fs'
-import * as path from 'path'
+import { format } from 'date-fns'
 
 export function createFile(config: Config) {
-  const outputPath = `${config.prjctNm}/${config.fileFrmt}.md`
-  // fs.promises.mkdir(path.resolve(__dirname, '/output'), {recursive: true})
+  const dateStr = format(new Date(), 'MM_dd_yyyy')
 
-  console.log(config.prjctNm)
+  console.log('date: ', dateStr)
+
+  const outputPath = `${config.prjctNm}/${dateStr}.md`
+
   // TODO: read template/config
   // TODO: use md template as config
   // TODO: gen files
   // TODO: gen headings
   // TODO: output config as part of generation
+  // TODO: add key for formatting
+
   fs.mkdirSync(config.prjctNm)
 
-  // fs.writeFile(path.resolve(__dirname, outputPath), 'bababa', {flag: 'w'}, err => {
-  //   if (err) {
-  //     console.error(err)
-  //   }
-  // })
-  fs.writeFile(outputPath, 'bababa', { flag: 'w' }, err => {
+  let iter = 1
+
+  switch (config.duration) {
+  case '100 days':
+    iter = 10
+    console.log('hey!')
+    break
+  case 'year':
+    console.log('hey!')
+    break
+  case 'month':
+    console.log('hey!')
+    break
+  case 'week':
+    console.log('hey!')
+    break
+  default:
+    console.log('hey!')
+    break
+  }
+
+  fs.writeFile(`${outputPath}`, 'bababa', err => {
     if (err) {
       console.error(err)
     }
@@ -26,6 +46,7 @@ export function createFile(config: Config) {
 }
 
 interface Config {
+  duration: '100 days' | 'year' | 'month' | 'week';
   fileFrmt: string;
   headingFrmt: string;
   prjctNm: string;
